d48a72848d9fc41914c25e92c137e5e48d0ab2ed
alejasu10 <alejasu0110@gmail.com>
2024-11-25 23:50:14 +0100

    act. agregado cifrado

--- test15.py
+++ test15.py
@@ -1,25 +1,4 @@
-###
-def incriptar(texto):
-    
-    for letra in texto:
-        print(ord(letra) + 1," ",end="")
-        
-def decriptar(texto):
-    
-    for letra in texto:
-        print(chr(letra-1))
-
-
-if __name__ == "__main__":
-    texto=input("Intruce tu texto para incriptar:  ")
-    palabra=[]
-incriptar(texto) 
-palabra.append(incriptar(texto))
-print(palabra)
-
-# no terminado
-
-
+### cifrar una palabra ejemplo-
 """ texto= "hola"
 numero=[]
 for i in texto:
@@ -27,4 +6,44 @@ for i in texto:
     numero.append(ord(i) + 1)
 for i in numero:
     print(chr(i-1)) """
+####
+# pedir al usuario que ingrese una palabra y cifrarla, despues decifrarla.
+# prog. de practica
+""" def encriptar(texto):
+    cifrado = []
+    for letra in texto:
+        cifrado.append(ord(letra) + 1)
+    return cifrado
+
+def descifrar(numeros):
+    texto_original = ""
+    for num in numeros:
+        texto_original += chr(num - 1)
+    return texto_original
+
+if __name__ == "__main__":
+    # Encriptado
+    texto = input("Introduce tu texto para encriptar: ")
+    codigo_cifrado = encriptar(texto)
+    
+    msg = " ".join(map(str, codigo_cifrado))# convierte en string el codigo cifrado con la funcion map
+    print("Texto cifrado:", msg)
+    
+    # Desifrado
+    texto_descifrado = descifrar(codigo_cifrado)
+    print("Texto descifrado:", texto_descifrado) """
+    
+# pedir al usuario que ingrese los numeros y decifrarlos
+""" def descifrar(numeros):
+    texto_original = ""
+    for num in numeros:
+        texto_original += chr(num - 1)
+    return texto_original
+
+if __name__ == "__main__":
+    # Desifrado
+    numeros = list(map(int, input("Introduce los numeros separados por espacios: ").split()))
+    texto_descifrado = descifrar(numeros)
+    print("Texto descifrado:", texto_descifrado) """
+##########
 
